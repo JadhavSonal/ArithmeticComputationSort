588aaf0b04d31182888b49828eb1177044eb2aa0
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-02 17:33:11 +0530

    Store Result in Dictionary

--- ArithmeticComputationSort.sh
+++ ArithmeticComputationSort.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+declare -A Arithmetic
 echo "Welcome in Arithmetic Computation Sort"
 read -p " Enter the values of a b & c =" a b c
 result1=$((a+b*c))
@@ -13,3 +14,9 @@ echo "Display the result of ((c+a/b)) = $result3"
 
 result4=$((a%b+c))
 echo "Display the result of ((a%b+c)) = $result4"
+
+Arithmetic[1]=$result1
+Arithmetic[2]=$result2
+Arithmetic[3]=$result3
+Arithmetic[4]=$result4
+echo "Number of Result in Dictionaries are = ${Arithmetic[@]}" 
