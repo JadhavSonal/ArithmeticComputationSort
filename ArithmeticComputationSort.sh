d2fdead0224d3d9785617f2c4dad27548b505f8f
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-02 17:14:58 +0530

    Compute Result of ((a*b+c))

--- ArithmeticComputationSort.sh
+++ ArithmeticComputationSort.sh
@@ -4,3 +4,6 @@ echo "Welcome in Arithmetic Computation Sort"
 read -p " Enter the values of a b & c =" a b c
 result1=$((a+b*c))
 echo "Display the result of ((a+b*c)) = $result1"
+
+result2=$((a*b+c))
+echo "Display the result of ((a*b+c)) = $result2"
