8774d36ec9d632d38d9225a7a9288944dcddeb2e
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-04 20:29:51 +0530

    Show No of times head and tail has won

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -13,3 +13,27 @@ else
         echo "its tail"
 fi
 
+echo "Shows number of times head and tails has won"
+
+tail=0
+while [ $head -ne 11 -a $tail -ne 11 ]
+        do
+                echo "Flipping coin"
+                toss=$((1+RANDOM%2))
+                if [ $toss -eq 1 ]
+                then
+                        echo "Heads"
+                        head=$(($head + 1))
+                elif [ $head -eq 2 ]
+                then
+                        echo "Tail"
+                        tail=$(($tail + 1))
+                fi
+                done
+                if  [ $head -eq 11 ]
+                then
+                        echo " Head wins the game "
+                else
+                        echo "Tails wins the game"
+        fi
+
