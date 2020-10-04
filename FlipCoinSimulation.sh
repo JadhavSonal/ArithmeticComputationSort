b0723db986ce88e87b0f885620f7b37f7a742bba
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-04 20:37:32 +0530

    Show if it's a Win or Tie

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -16,7 +16,7 @@ fi
 echo "Shows number of times head and tails has won"
 
 tail=0
-while [ $head -ne 11 -a $tail -ne 11 ]
+while [ $head -ne 5 -a $tail -ne 5 ]
         do
                 echo "Flipping coin"
                 toss=$((1+RANDOM%2))
@@ -37,3 +37,26 @@ while [ $head -ne 11 -a $tail -ne 11 ]
                         echo "Tails wins the game"
         fi
 
+echo "Shows if it's a win or tie if win then who won and by how much"
+
+tail=0
+while [ $head -ne 21 -a $tail -ne 21 ]
+        do
+                echo "Flipping coin"
+                toss=$((1+RANDOM%2))
+                if [ $toss -eq 1 ]
+                then
+                        echo "Heads"
+                        head=$(($head + 1))
+                elif [ $head -eq 2 ]
+                then
+                        echo "Tail"
+                        tail=$(($tail + 1))
+                fi
+                done
+                if  [ $head -eq 11 ]
+                then
+                        echo " Head wins the game "
+                else
+                        echo "Tails wins the game"
+        fi
