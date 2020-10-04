1d11e5d6882b30a8f7a5aa87dfc1ec3bce4675f5
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-04 16:00:22 +0530

    Display Heads or Tails as winner

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,3 +2,14 @@
 
 echo "Welcome in Flip Coin Simulation"
 
+echo "Filpping a coin to display Heads or Tails"
+
+head=1
+randomCheck=$(( RANDOM%2 ))
+if [ $head -eq $randomCheck ]
+then
+        echo "its head"
+else
+        echo "its tail"
+fi
+
