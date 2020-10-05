118f3ddb07efb3b07760ebe3743299c126b3fcf8
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-05 15:25:01 +0530

    welcome in User Registration

--- User_Registration.sh
+++ User_Registration.sh
@@ -1,12 +1,4 @@
 #!/bin/bash -x
 
 echo "Welcome in User Registration"
-read -p "Enter a First Name"  FirstName
-FnamePattern="^([A-Z]*[a-z]){3}$"
-if [[ $FirstName =~ $FnamePattern ]]
-then
-        echo "Valid First Name"
-else
-        echo "Invalid First Name"
-fi
 
