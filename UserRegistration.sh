766b32c7b841c5417127767e6bf00dcca921877c
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-05 05:53:37 +0530

    User Need to enter valdi Fname

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -x
 
 echo "Welcome in User Registration"
-
+read -p "Enter a First Name"  FirstName
+FnamePattern="^([A-Z]*[a-z]{3}$"
+if [[ $FirstName =~ $FnamePattern ]]
+then
+	echo "Valid First Name"
+else
+	echo "Invalid First Name"
+fi
