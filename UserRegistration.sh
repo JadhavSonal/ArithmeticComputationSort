5b1f1fb052c206d6ebba67003a15ef7b642fc0cf
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-05 16:12:37 +0530

    Enter Valid First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -x
 
 echo "Welcome in User Registration"
-
+read -p "Enter valid First Name" fname
+FNamePat="^([A-Z]*[a-z]){3}$"
+if [[ $fname =~ $FNamePat ]]
+then 
+	echo "Valid First name"
+else
+	echo "Invalid First name"
+fi 
